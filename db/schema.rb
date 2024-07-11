35aafaaf39d3eec899fc332ec54b210c2f2d3c8e
gregoftheweb <greg.obyrne@live.com>
2024-07-10 18:01:54 -0700

    finished tutorial

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_08_235417) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_10_233146) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "public"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status", default: "public"
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "komments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_komments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
+  add_foreign_key "komments", "articles"
 end
